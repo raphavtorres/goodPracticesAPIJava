5680cb8102617b972d9358ced3c68430fc0cfbe9
raphavtorres <raphaeltorresprofissional@gmail.com>
2024-02-16 11:37:46 -0300

    feat: added readme docs

--- src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
+++ src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
@@ -95,6 +95,7 @@ public class AdocaoController {
         email.setText("Olá " +adocao.getTutor().getNome() +"!\n\nInfelizmente sua adoção do pet " +adocao.getPet().getNome() +", solicitada em " +adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) +", foi reprovada pelo abrigo " +adocao.getPet().getAbrigo().getNome() +" com a seguinte justificativa: " +adocao.getJustificativaStatus());
         emailSender.send(email);
 
+
         return ResponseEntity.ok().build();
     }
 
